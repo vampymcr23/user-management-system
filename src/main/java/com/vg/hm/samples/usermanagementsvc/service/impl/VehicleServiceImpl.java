41c5582af39b1426b0cdde63d8cd60a49931e8b2
unknown <geormerla@gmail.com>
2021-01-20 15:41:11 -0500

    changes requested

--- src/main/java/com/vg/hm/samples/usermanagementsvc/service/impl/VehicleServiceImpl.java
+++ src/main/java/com/vg/hm/samples/usermanagementsvc/service/impl/VehicleServiceImpl.java
@@ -8,6 +8,7 @@ import com.vg.hm.samples.usermanagementsvc.service.VehicleService;
 import com.vg.hm.samples.usermanagementsvc.service.model.Make;
 import com.vg.hm.samples.usermanagementsvc.service.model.MakesJson;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import retrofit2.Call;
 import retrofit2.Response;
@@ -20,23 +21,20 @@ import java.util.List;
 @Log4j2
 @Service
 public class VehicleServiceImpl implements VehicleService {
-    public static final String BASE_URL = "https://vpic.nhtsa.dot.gov/api/vehicles/";
-    Gson gson = new GsonBuilder().setDateFormat("yyy-MM-dd'T'HH:mm:ssz").create();
-    Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create(gson)).build();
-    VehicleAPIService vehicleAPIService = retrofit.create(VehicleAPIService.class);
+    @Autowired
+    VehicleAPIService vehicleRetrofitClient;
 
     @Override
     @HystrixCommand(fallbackMethod = "getDummyMakes")
     public List<Make> getMakes() throws IOException {
        log.info("Getting all makes...");
-        Call<MakesJson> call = vehicleAPIService.getAllMakes();
+        Call<MakesJson> call = vehicleRetrofitClient.getAllMakes();
         Response<MakesJson> response = call.execute();
         MakesJson makes  = response.body();
         List<Make> makeList= makes.getResults();
         return makeList;
     }
 
-    @Override
     public List<Make> getDummyMakes(){
         log.info("Getting dummy makes");
         List<Make> makeList = new ArrayList<>();
